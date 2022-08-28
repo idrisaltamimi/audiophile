e51543cb14fd86e8f650276c9f59ef13186fce8c
idrisaltamimi <idris.al.tamimi.1@gmail.com>
2022-08-28 14:26:36 +0400

    c

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 import './index.css'
 import App from './App'
@@ -9,9 +9,9 @@ import App from './App'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
    <React.StrictMode>
-      <HashRouter basename='/audiophile'>
+      <Router basename='/audiophile'>
          <App />
-      </HashRouter>
+      </Router>
    </React.StrictMode>
 )
 
